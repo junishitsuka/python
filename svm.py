d88f849d8c3fef0ca8fc3f11741909bf358e533a
Jun Ishitsuka <ishitsuka.jun@gmail.com>
2014-06-24 00:35:17 +0900

    sample svn using scikit-learn

---
+++ svm.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# coding: utf-8
+
+from sklearn import svm
+
+X = [[-4,-4,-1],[-2,-2,0],[1,1,2],[3,3,5]] #入力事例
+y = [1,1,0,0] #クラスラベル
+clf = svm.SVC(kernel='rbf') #Support Vector Classification(分類)、RBFカーネルを使用
+clf.fit(X, y) #学習
+result = clf.predict([2,2,4]) #予測
+
+print result
