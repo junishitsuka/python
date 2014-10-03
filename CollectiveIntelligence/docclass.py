7057b9f9b8a71983e03f50f0c53e9dd0c4321a08
junishitsuka <ishitsuka.jun@gmail.com>
2014-10-03 11:26:02 +0900

    fisher classifier

--- CollectiveIntelligence/docclass.py
+++ CollectiveIntelligence/docclass.py
@@ -93,6 +93,51 @@ class naivebayes(classifier):
             if cat == best: continue
             if probs[cat] * self.getthreshold(best) > probs[best]: return default
         return best
+
+class fisherclassifier(classifier):
+    def __init__(self, getfeatures):
+        classifier.__init__(self, getfeatures)
+        self.minimums = {}
+
+    def cprob(self, f, cat):
+        clf = self.fprob(f, cat)
+        if clf == 0: return 0
+
+        freqsum = sum([self.fprob(f, c) for c in self.categories()])
+        return clf / freqsum
+
+    def fisherprob(self, item, cat):
+        p = 1
+        features = self.getfeatures(item)
+        for f in features:
+            p *= self.weightedprob(f, cat, self.cprob)
+        fscore = -2 * math.log(p)
+        return self.invchi2(fscore, len(features) * 2)
+
+    def invchi2(self, chi, df):
+        m = chi / 2.0
+        sum = term = math.exp(-m)
+        for i in range(1, df//2):
+            term *= m / i
+            sum += term
+        return min(sum, 1.0)
+
+    def setminimum(self, cat, min):
+        self.minimums[cat] = min
+
+    def getminimum(self, cat):
+        if cat not in self.minimums: return 0
+        return self.minimums[cat]
+
+    def classify(self, item, default=None):
+        best = default
+        max = 0.0
+        for c in self.categories():
+            p = self.fisherprob(item, c)
+            if p > self.getminimum(c) and p > max:
+                best = c
+                max = p
+        return best
             
     
 def sampletrain(cl):
